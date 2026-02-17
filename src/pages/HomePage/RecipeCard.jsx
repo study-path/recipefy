18cb74c25297c0b9c53514bc78aa84f4cf82026b
study-path <liliyaingul@gmail.com>
2026-02-17 00:20:46 -0500

    Improve layout of recipe card

--- src/pages/HomePage/RecipeCard.jsx
+++ src/pages/HomePage/RecipeCard.jsx
@@ -1,16 +1,12 @@
 import { Link } from "react-router";
 const RecipeCard = ({ recipe }) => {
   return (
-    <div className="flex flex-wrap justify-center ">
-      <Link
-        to={`/recipeDetail/${recipe.id}`}
-        key={recipe.id}
-        className="flex flex-col m-1 border-2 rounded-md w-[240px] border-gray-300 hover:shadow-xl  "
-      >
+    <div className="border-2 border-gray-300 h-full w-[230px] hover:shadow-xl rounded-md">
+      <Link to={`/recipeDetail/${recipe.id}`} key={recipe.id}>
         <img
           src={recipe.image}
           alt={recipe.title}
-          className="h-50 object-cover rounded-t-md "
+          className="h-40 w-full object-cover rounded-t-xs "
         ></img>
         <div className="text-xl m-2 ">{recipe.title}</div>
       </Link>
