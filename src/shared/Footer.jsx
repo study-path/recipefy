0b99a1a5af4df349f44c320add247d46f5299d36
study-path <liliyaingul@gmail.com>
2026-08-07 15:01:57 -0400

    fix

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-300 py-4 text-center  ">
       <p className="text-gray-600 font-bubblegum text-2xl">
-        © {new Date().getFullYear()} Recipi
+        © {new Date().getFullYear()} Recipe
         <span className="text-lime-500">fy</span> App
       </p>
     </footer>
